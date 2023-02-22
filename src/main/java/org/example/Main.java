8aaedb39dbda9fced05ed9b0ce7db83b5856675b
osawa-koki <github@osawa.dev>
2023-02-22 19:54:18 +0900

    Shuffled

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,17 @@
 package main.java.org.example;
 
 import static main.java.org.example.Display.Display;
+import static main.java.org.example.Shuffle.Shuffle;
 
 public class Main {
   public static void main(String[] args) {
 
     int[] ary = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-    Display("original", ary);
+    Display("Original", ary);
+
+    Shuffle(ary);
+    Display("Shuffled", ary);
 
   }
 }
---
+++ src/main/java/org/example/Shuffle.java
@@ -0,0 +1,12 @@
+package main.java.org.example;
+
+public class Shuffle {
+  public static void Shuffle(int[] ary) {
+    for (int i = 0; i < ary.length; i++) {
+      int j = (int) (Math.random() * (i + 1));
+      int temp = ary[i];
+      ary[i] = ary[j];
+      ary[j] = temp;
+    }
+  }
+}
