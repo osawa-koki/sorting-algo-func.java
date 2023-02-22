40f2aef9a6487b973841711a644a7e31be24731e
osawa-koki <github@osawa.dev>
2023-02-22 20:06:10 +0900

    整列されているかどうかを出力。

--- src/main/java/org/example/Display.java
+++ src/main/java/org/example/Display.java
@@ -1,7 +1,15 @@
 package main.java.org.example;
 
+import static main.java.org.example.IsSorted.IsSorted;
+
 public class Display {
   public static void Display(String title, int[] ary) {
-    System.out.println(String.format("%-20s", title) + ": " + String.join(" ", java.util.Arrays.stream(ary).mapToObj(String::valueOf).toArray(String[]::new)));
+    String isSorted = IsSorted(ary) ? "\u001B[34m" + "true " + "\u001B[0m" : "\u001B[31m" + "false" + "\u001B[0m";
+    System.out.println(String.format(
+      "%s (%s) : %s",
+      String.format("%-20s", title),
+      (isSorted),
+      String.join(" ", java.util.Arrays.stream(ary).mapToObj(String::valueOf).toArray(String[]::new))
+    ));
   }
 }
---
+++ src/main/java/org/example/IsSorted.java
@@ -0,0 +1,12 @@
+package main.java.org.example;
+
+public class IsSorted {
+  public static boolean IsSorted(int[] ary) {
+    for (int i = 1; i < ary.length; i++) {
+      if (ary[i] < ary[i - 1]) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
